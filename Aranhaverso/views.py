087ed58840586ec115abc0ffd8b23d8fa0a33923
JuninMercadorias <yasminoliveira072211@gmail.com>
2025-04-11 14:03:43 -0300

    update

--- Aranhaverso/views.py
+++ Aranhaverso/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'Aranhaverso/index.html')
+
+def sobre(request):
+    return render(request, 'Aranhaverso/sobre.html')
